d62b0e229489b0fb25eec379abf90360effae8c1
ALAN SHAJAN <tve24el006cet.ac.in>
2026-02-06 11:10:35 +0530

    forty_fifthcommit"

--- src/MethodOverriding.java
+++ src/MethodOverriding.java
@@ -1,4 +1,8 @@
 class Employee {
+    void display()
+    {
+        System.out.println("Name of class is Employee");
+    }
    
 }
 
@@ -6,11 +10,19 @@ class Engineer extends Employee {
     // TODO: Override display() method
     // 1. Print "Name of class is Engineer"
     // 2. Call super.display() to print the parent's message
+    void display()
+    {
+        System.out.println("Name of class is Engineer");
+        super.display();
+    }
 }
 
 public class MethodOverriding {
-   
+    public static void main(String[] args) {
         // TODO: Create an Engineer object
         // TODO: Call the display() method
-    
+        Engineer E1 = new Engineer();
+        E1.display();
+    }
 }
+        
